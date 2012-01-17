14c9b27a5a1a9ba7f3e391bae0cddcd209507da3
Jake Drahos <jake@laptop.(none)>
2012-01-17 14:33:38 -0600

    Added pony.

--- teleop.c
+++ teleop.c
@@ -84,6 +84,60 @@ task main()
 
 		servo[servoC] = clampServo; //clamp servo = clamp servo value
 		
-		
+		/*
+		//===========================================\\
+		||========---------BEGIN PONY--------========||
+		\\===========================================//	
+
+
+                                                                                                                           
+                                                   ._.       _i,_.                                                         
+                                            .__iaa%^-      _wmZ##qoa,,                                                     
+                                       .__awwm#Y":____====vSYYXXX##Z#mas,                                                  
+                                   ._<awmWm#X1vvvnvnnoooonnvvvvvvv1XXZ#Zqoi.                                               
+                                ._awmmWUS1nvnvnvvvnvnvnvnvvvvvvnvnvvvv3X#Zmos,                                             
+                              _iwmm#AY1vvnnnvnvnvnnvnvnvvvn2SXXZZ#U#Z#X2X#Z#Xoi.                                           
+                            _iqmm#S1nvvnvnvvnvvvIIIIIlliIlIlllIvv1YSX#ZZ2X#UZ#oa.                                          
+                           =wmmBSvnvnvnvIIili|i|ii|i||i|i||i|i|i|iiiiI1XoX#Z###Xc                                          
+              ____.       =q#mXnvvvIll|i|i|i|i|i|i|i|i|i|ii|i|i|i|i|i|i|lIXZ#Z#Xo;                                         
+            =wqmqqqwoai,,.im#SvIliii|i|i|i|i|i|i|i|i|i|i|i|i|i|i|i|i|i|i|iiIX##UX(                                         
+           =X#UZ#UZ##Z#mqwoU1i||i|i|i|i|i|i|iiiviiiiliiiiii|i|i|i|i|i|i|i|iii3UZXc                                         
+           vXZ##Z##Z##Z#Z#Zmooviiiiiiiiii|iiiiI3XU#UZUZr-=2^-+++++++||v%iii|iiXZo'                                         
+           {XZ#Z#UZ#UZ#UUU#Z##mXqqqqqqqwo2++vaasivX#U#Z(_Z`<gQm/ . --.-?$mc-{ouS(                                          
+           )o#Z##ZX#Z##Z#Z#Z##ZZX??!"""--.+?S#Z#ZmXUZ#U##k dQQQf _s,  -. )Qa.)#o,                                          
+           -1XZ#Z#XX#Z#UUUUX#ZZa%: __wwwgggw,+X#Z#Z##Z#Z#Zc"??" =$H(    - +Qc.X#X>                                         
+            -1X##Z#X2XZ#Z#Za%|}~ _yWV!^~-+"?HqZ##Z##Z#UUZ#Zc.;..         : 3f_m#ZX>                                        
+             -{XZ##UUX2XXXY!!"` jWY=awwc  -..+YZ#U#Z#Z#Z##U#qc.:.      ..  =<d#Z#Uo;                                       
+               ~{X#Z#Z#qXXowu( <W(_QWWW[     : ]#Z#Z##ZZXXXXXX#wai_s_i_%_aw###Z##ZX(                                       
+                 "*XZ##Z#ZZ|_c jW.-YYT" vyw.  :-X##UXSXZ##UZSd#ZZ##ZXZmq#mX#Z#U#Z#S(                                       
+                   -!1XXUU####,+Qc :    -"~   - 3#Z#XZXXXZ#UZ#ZXXSXqZ#ZUZ#Z##Z#Z#Xe`                                       
+                      -"*1SSXX6.]m> :        . .mZ##U####ZZXXoqqZ#Z#UU#Z##Z#Z#UUXe`                      ___iiii=>,        
+                          -{ooon,"Vw,:..    - _w#UUXXUXX2XqmX##Z#Z#Z#Z#U#Z##UUZSos__.               ._sawqmZ#ZZZ#mXa,      
+                           =2S22Sas=?!=:...__wm##Z#X2XqZ#UZZ#Z#U##U#Z#UZ#Z#ZZXoXZZmqwos,         _=uqmZZ#ZZ##U##ZZUZXs.    
+                           ivvSo2oXXmowuwuq#m##Z#UU#ZUZ#UZ##Z##Z#ZUZ##Z##ZZXSX####ZZ#Z#ma=.   ._uX##ZUZ##U#Z#Z#Z#####Xa.   
+                          .>illv1SooXXZ#Z#Z#Z#U#Z#Z#Z##UZ##Z##Z#Z#U#Z#ZXXSXmX#ZUZ#Z###ZZ#ma,_iumZ#Z##U##Z#Z##Z##Z#ZUZ#Za,  
+                          =|=i+illIv1o2XXXqXqXXXXX#Z##Z##UZ##Z#Z#XXXXXqmZ#XX#U#Z##U#Z#U#ZZ#mo2XZ###Z#Z#Z#Z##Z##Z##Z##Z#Xa. 
+            .....         --:|==+|inwmZ#U#Z#Z#UZ#qXXX2XX2XSSSSXXqqZ##UZUZ#Zm2XZ##Z#Z#Z#Z##ZZ#moXX#Z#UUUUU#UZ#UZ#UZ##Z###X; 
+        __awwqqwwoas,_.     ->==<iwmXZ#Z#Z##UUZ##Z#XXXZZZZZ#Z#Z#Z##Z#Z##UU#Z#moXX#UU#UU#Z###ZZ##UZ#UZ#Z#Z#Z##Z##Z#ZZXXS1"` 
+     .<um##UZ#ZXZ##Z#moa,,   ==inmXZ##U#Z#UZ#Z##Z#Z#Z######Z#Z##Z#Z##UUZ#Z#Z#Z#moX#ZUZ#Z##Z###Z#Z##Z##UU#Z##Z#XX1!""--     
+   .<wmZ#Z#Z#Xo#Z#Z##Z##mXa,, )XmX##Z#Z#U#Z##Z#Z###Z##Z#ZUZ###Z##Z##Z#Z##Z##Z#Z#UX2X##Z##Z#Z#Z##Z#Z##Z#Z#Z#X1!~-           
+  .uXZUZ##Z#XX#Z##U#Z#UZUZ#moao#Z#Z##Z#UZ#UUZ###Z#Z#XSX#Z##Z#Z#UZ##Z#Z##Z##Z###Z##moXXX#Z##UUUUZ###Z#UZ#Xe(`               
+  vX##U#Z#Xo#Z#UUZ#Z#Z##Z#ZZZXXX#Z##Z##Z#UZ##Z#Z#ZSoXmZ#U#Z##Z#Z##Z###Z#UUZ#UZ#Z#ZZ#on>!YXZUZ#Z##Z#UZXe!~                  
+ :XZ#ZUZXXmZ##Z#Z#UU#UUZ#####ZXo##Z#Z#Z##Z#UZ#ZZS}^{nXZ#Z#Z#Z###Z#UUZ#Z#Z##Z##Z##U#Z#Xn. -"!YSXXXXS*"`                     
+ -nX#XXqZ#Z#UZ##Z#Z#Z#Z##Z#Z#UmXZ#U#U#U#Z##Z#Xo}`   {2#Z#U###Z#Z##Z#UU#U#Z#UUZ##Z#Z##ZXn.     ---                          
+  -"!?XXUZ#Z##Z##U#Z#U#Z#Z#UZ#ZZ#Z#Z#Z#Z#UZX1"`     -Sn##Z#ZU#Z##Z#Z#Z#Z#UZ#Z##Z#Z##Z#ZXs                                  
+       -"!1XX#UZ#Z##Z#Z##U#Z##Z###Z#UZ#UZX*^         )oZ#UZ#UZ##Z#U#Z#UUZ##Z##Z#U#Z#U#Zmn>                                 
+           -"!1XX#Z#U#UUZ#Z##Z##Z#U#Z#ZS*`           -nX#Z##Z##Z#UZ##Z#Z##Z##Z#UZ##Z#Z#ZXn,                                
+               -"*SXXUZ##Z#UZ#UZ#Z#Z#X}`              {X##Z##Z#UUZ#UZ###Z#UZ#UUZ#UZ##Z###Xc                                
+                   -"!?XXZ#Z#UZ##UZX!`                )nX#UUZ##Z##Z##Z#Z#UZ##Z##Z##Z##Z#ZXn;                               
+                        -^"!!?YY*!"`                  :oX#Z##Z#Z#Z##Z##Z#Z##Z#Z##Z##Z#UU##X(                               
+                                                      :oX##Z#U#U#UUZ#UZ###Z##Z##Z#UZ#Z#Z#ZXn.                              
+                                                      :2X#Z#UZ#Z#Z##Z##Z#Z##Z##Z#Z##Z##UZ##o;                              
+		//===========================================\\
+		||========----------END PONY---------========||
+		\\===========================================//	
+
+*/
+
 }
 
